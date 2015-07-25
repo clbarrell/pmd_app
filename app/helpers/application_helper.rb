fd5f9bbd32ef6668ced3d12c5f8ec4e2cb3b62a5
Chris Barrell <clbarrell@gmail.com>
2015-07-25 00:03:46 +0000

    Setup the home and help pages, no content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    
+    # Makes the title
+    def full_title(page_title = '')
+       base = "PMD App - FBG Group" 
+      if page_title.empty?
+        base
+      else
+        page_title + " | " + base
+      end
+    end
+    
 end
